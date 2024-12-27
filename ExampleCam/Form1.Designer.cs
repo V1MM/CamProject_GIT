9ff954c44bf91a7c6bdea6f2ac338829176dbf99
nenea <nenea@NESUKO_LAPTOP>
2024-12-27 15:41:16 +0700

    Add project files.

---
+++ ExampleCam/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ExampleCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            groupBoxView = new GroupBox();
+            imageBoxFace = new Emgu.CV.UI.ImageBox();
+            imageBoxLive = new Emgu.CV.UI.ImageBox();
+            groupBoxControl = new GroupBox();
+            buttonFlipH = new Button();
+            buttonFlipV = new Button();
+            buttonStart = new Button();
+            buttonConnect = new Button();
+            CamStatus = new Label();
+            RecStatus = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusLabelClock = new Label();
+            statusLabelDate = new Label();
+            groupBoxView.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)imageBoxFace).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)imageBoxLive).BeginInit();
+            groupBoxControl.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBoxView
+            // 
+            groupBoxView.Controls.Add(imageBoxFace);
+            groupBoxView.Controls.Add(imageBoxLive);
+            groupBoxView.Location = new Point(137, 31);
+            groupBoxView.Name = "groupBoxView";
+            groupBoxView.Size = new Size(830, 390);
+            groupBoxView.TabIndex = 0;
+            groupBoxView.TabStop = false;
+            groupBoxView.Text = "Live View";
+            // 
+            // imageBoxFace
+            // 
+            imageBoxFace.Location = new Point(612, 90);
+            imageBoxFace.Name = "imageBoxFace";
+            imageBoxFace.Size = new Size(198, 246);
+            imageBoxFace.TabIndex = 2;
+            imageBoxFace.TabStop = false;
+            // 
+            // imageBoxLive
+            // 
+            imageBoxLive.Location = new Point(6, 26);
+            imageBoxLive.Name = "imageBoxLive";
+            imageBoxLive.Size = new Size(572, 358);
+            imageBoxLive.TabIndex = 2;
+            imageBoxLive.TabStop = false;
+            // 
+            // groupBoxControl
+            // 
+            groupBoxControl.Controls.Add(buttonFlipH);
+            groupBoxControl.Controls.Add(buttonFlipV);
+            groupBoxControl.Controls.Add(buttonStart);
+            groupBoxControl.Controls.Add(buttonConnect);
+            groupBoxControl.Location = new Point(286, 439);
+            groupBoxControl.Name = "groupBoxControl";
+            groupBoxControl.Size = new Size(538, 142);
+            groupBoxControl.TabIndex = 1;
+            groupBoxControl.TabStop = false;
+            groupBoxControl.Text = "Control Panel";
+            groupBoxControl.Enter += groupBoxControl_Enter;
+            // 
+            // buttonFlipH
+            // 
+            buttonFlipH.Location = new Point(309, 38);
+            buttonFlipH.Name = "buttonFlipH";
+            buttonFlipH.Size = new Size(94, 83);
+            buttonFlipH.TabIndex = 5;
+            buttonFlipH.Text = "Flip H";
+            buttonFlipH.UseVisualStyleBackColor = true;
+            buttonFlipH.Click += buttonFlipH_Click;
+            // 
+            // buttonFlipV
+            // 
+            buttonFlipV.Location = new Point(418, 38);
+            buttonFlipV.Name = "buttonFlipV";
+            buttonFlipV.Size = new Size(94, 83);
+            buttonFlipV.TabIndex = 4;
+            buttonFlipV.Text = "Flip V";
+            buttonFlipV.UseVisualStyleBackColor = true;
+            buttonFlipV.Click += buttonFlipV_Click;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(181, 38);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(97, 83);
+            buttonStart.TabIndex = 2;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonConnect
+            // 
+            buttonConnect.Location = new Point(68, 38);
+            buttonConnect.Name = "buttonConnect";
+            buttonConnect.Size = new Size(98, 83);
+            buttonConnect.TabIndex = 0;
+            buttonConnect.Text = "Connect";
+            buttonConnect.UseVisualStyleBackColor = true;
+            buttonConnect.Click += buttonConnect_Click;
+            // 
+            // CamStatus
+            // 
+            CamStatus.AutoSize = true;
+            CamStatus.BackColor = Color.RosyBrown;
+            CamStatus.Location = new Point(986, 561);
+            CamStatus.Name = "CamStatus";
+            CamStatus.Size = new Size(129, 20);
+            CamStatus.TabIndex = 2;
+            CamStatus.Text = "Status: Disconnect";
+            // 
+            // RecStatus
+            // 
+            RecStatus.AutoSize = true;
+            RecStatus.BackColor = Color.RosyBrown;
+            RecStatus.Location = new Point(986, 583);
+            RecStatus.Name = "RecStatus";
+            RecStatus.Size = new Size(104, 20);
+            RecStatus.TabIndex = 3;
+            RecStatus.Text = "Recording: No";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // statusLabelClock
+            // 
+            statusLabelClock.AutoSize = true;
+            statusLabelClock.Location = new Point(986, 9);
+            statusLabelClock.Name = "statusLabelClock";
+            statusLabelClock.Size = new Size(50, 20);
+            statusLabelClock.TabIndex = 4;
+            statusLabelClock.Text = "label1";
+            // 
+            // statusLabelDate
+            // 
+            statusLabelDate.AutoSize = true;
+            statusLabelDate.Location = new Point(986, 31);
+            statusLabelDate.Name = "statusLabelDate";
+            statusLabelDate.Size = new Size(50, 20);
+            statusLabelDate.TabIndex = 5;
+            statusLabelDate.Text = "label2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1123, 612);
+            Controls.Add(statusLabelDate);
+            Controls.Add(statusLabelClock);
+            Controls.Add(RecStatus);
+            Controls.Add(CamStatus);
+            Controls.Add(groupBoxControl);
+            Controls.Add(groupBoxView);
+            Name = "Form1";
+            Text = "z";
+            groupBoxView.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)imageBoxFace).EndInit();
+            ((System.ComponentModel.ISupportInitialize)imageBoxLive).EndInit();
+            groupBoxControl.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBoxView;
+        private GroupBox groupBoxControl;
+        private Button buttonFlipH;
+        private Button buttonFlipV;
+        private Button buttonStart;
+        private Button buttonConnect;
+        private Emgu.CV.UI.ImageBox imageBoxLive;
+        private Label CamStatus;
+        private Label RecStatus;
+        private System.Windows.Forms.Timer timer1;
+        private Emgu.CV.UI.ImageBox imageBoxFace;
+        private Label statusLabelClock;
+        private Label statusLabelDate;
+    }
+}
---
+++ ExampleCam/Form1.cs
@@ -0,0 +1,267 @@
+using System.Drawing.Printing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ExampleCam
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture? _capture = null;
+        private Mat? _frame;
+        private bool IsConnect = true;
+        private bool IsRecord = true;
+
+        CascadeClassifier _cascadeClassifier = new CascadeClassifier(@"C:\Users\nenea\Downloads\emgucv-master\emgucv-master\opencv\haarcascade_frontalface_default.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonStart.Enabled = false;
+            buttonFlipV.Enabled = false;
+            buttonFlipH.Enabled = false;
+
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                using (var imageFrame = _capture.QueryFrame().ToImage<Bgr, Byte>())
+                {
+                    if (imageFrame != null)
+                    {
+                        var grayframe = imageFrame.Convert<Gray, byte>();
+                        var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10);
+
+                        foreach (var face in faces)
+                        {
+                            imageFrame.Draw(face, new Bgr(Color.MistyRose), 3);
+                        }
+                        imageBoxLive.Image = imageFrame;
+
+                        if (faces.Length > 0)
+                        {
+                            Rectangle face_roi = new Rectangle(faces[0].X, faces[0].Y, 190, 190);
+                            grayframe.ROI = face_roi;
+                            imageBoxFace.Image = grayframe.Copy();
+                        }
+                    }
+
+                    //bool canCapture = _capture.Retrieve(_frame, 0);
+                    //if (canCapture)
+                    //{
+
+                    //    imageBox1.Image = _frame;
+                    //}
+
+                }
+            }
+        }
+
+
+        private async void buttonConnect_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (IsConnect)
+            {
+                await WaitConnectCam();
+                await Connect();
+
+                buttonConnect.Text = "Disconnect";
+
+                buttonStart.Enabled = true;
+                buttonFlipV.Enabled = true;
+                buttonFlipH.Enabled = true;
+            }
+            else
+            {
+                Disconnect();
+                buttonConnect.Text = "Connect";
+                buttonStart.Enabled = false;
+                buttonFlipV.Enabled = false;
+                buttonFlipH.Enabled = false;
+
+            }
+            IsConnect = !IsConnect;
+
+
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (_capture != null)
+                {
+                    _capture.Pause();
+                    _capture.ImageGrabbed -= ProcessFrame;
+                    _capture.Dispose();
+
+                }
+                if (_frame != null)
+                {
+                    _frame.Dispose();
+                }
+
+                DisconnectCam();
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+        }
+
+        private void Disconnect()
+        {
+            try
+            {
+                if (_capture != null)
+                {
+                    _capture.Pause();
+                    _capture.ImageGrabbed -= ProcessFrame;
+                    _capture.Dispose();
+
+                }
+                if (_frame != null)
+                {
+                    _frame.Dispose();
+                }
+
+                DisconnectCam();
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+        }
+
+        private async Task Connect()
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    _capture = new VideoCapture();
+                    _frame = new Mat();
+
+                });
+
+                if (_capture != null)
+                {
+                    _capture.ImageGrabbed += ProcessFrame;
+                    ConnectCam();
+                }
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+            if (IsRecord)
+            {
+
+                if (_capture != null)
+                {
+                    _capture.Start();
+                }
+                Recording();
+                buttonStart.Text = "Pause";
+                buttonConnect.Enabled = false;
+            }
+            else
+            {
+                if (_capture != null)
+                {
+                    _capture.Pause();
+                }
+                NoRecording();
+                buttonStart.Text = "Start";
+                buttonConnect.Enabled = true;
+            }
+            IsRecord = !IsRecord;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                _capture.Pause();
+            }
+        }
+
+        private void buttonFlipV_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                _capture.FlipHorizontal = !_capture.FlipHorizontal;
+            }
+
+        }
+
+        private void buttonFlipH_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                _capture.FlipVertical = !_capture.FlipVertical;
+            }
+
+        }
+
+        private async void ConnectCam()
+        {
+            CamStatus.Text = "Status:Connect";
+            CamStatus.BackColor = Color.LimeGreen;
+        }
+
+        private async Task WaitConnectCam()
+        {
+            CamStatus.Text = "Status:Waiting";
+            CamStatus.BackColor = Color.CadetBlue;
+            await Task.Delay(1000);
+        }
+
+        private async void DisconnectCam()
+        {
+            CamStatus.Text = "Status: Disconnect";
+            CamStatus.BackColor = Color.RosyBrown;
+        }
+
+        private async void Recording()
+        {
+            RecStatus.Text = "Recording: Yes";  
+            RecStatus.BackColor = Color.LimeGreen;
+        }
+
+        private async void NoRecording()
+        {
+            RecStatus.Text = "Recording: No";
+            RecStatus.BackColor = Color.RosyBrown;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string formatStringClock = "HH:mm:ss";
+            string formatStringDate = "yyyy-MMM-dd";
+
+            DateTime dateNow = DateTime.Now;
+            statusLabelClock.Text = dateNow.ToString(formatStringClock);
+            statusLabelDate.Text = dateNow.ToString(formatStringDate);
+
+        }
+
+        private void groupBoxControl_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
